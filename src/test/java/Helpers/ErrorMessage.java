48f8ff3e506223c6800d0cab4cc5d75f2c3161de
pakhomova_ev <pakhomova.e@gmail.com>
2025-02-18 13:30:08 +0300

    add hw1 test

---
+++ src/test/java/Helpers/ErrorMessage.java
@@ -0,0 +1,6 @@
+package Helpers;
+
+public class ErrorMessage {
+    public static final String FRAGILE_PACKAGE_ERROR = "Fragile cargo cannot be delivered for the distance more than 30";
+    public static final String DESTINATION_DISTANCE_POSITIVE_NUMBER = "destinationDistance should be a positive number!";
+}
